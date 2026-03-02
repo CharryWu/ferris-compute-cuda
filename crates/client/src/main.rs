b509901b533ceea474a1b2c66b6bc15cfc41b9dd
Charry Wu <zichao_wu@intuit.com>
2026-03-02 02:14:17 -0800

    fix(client): rust compile ownership issue

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -41,7 +41,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let request = tonic::Request::new(ComputeRequest {
         source_code,
-        file_name,
+        file_name: file_name.clone(),
         compiler_flags: args.flags,
     });
 
