57d622efaff5b1c3382c5d1db1e4880727fe437d
Charry Wu <zichao_wu@intuit.com>
2026-03-11 03:20:29 -0700

    fix(CI): add github token to avoid rate limit

--- crates/common/build.rs
+++ crates/common/build.rs
@@ -1,3 +1,5 @@
+/// Custom build script that extends the default build script provided by tonic-build.
+/// Requires `protoc` command to be available. See .github/workflows/ci.yml for more details.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // This compiles the .proto file into Rust code.
     // By default, the generated code is placed in the 'OUT_DIR' 
